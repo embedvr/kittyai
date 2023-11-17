d9fe2addab687bd211eadfd4d31ce3ff0d8363e0
embed <brydon@lecla.ir>
2023-11-17 13:09:02 -0500

    [~] new landing page

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,7 +1,48 @@
-<h1>helium ai</h1>
+<script lang="ts">
+	import { goto } from '$app/navigation';
 
-<h2>catgirl route</h2>
-<h3>https://ai.helium.ws/cat/[prompt]</h3>
+	let cgInput: HTMLInputElement;
+	let pgInput: HTMLInputElement;
+	let loading = false;
 
-<h2>puppygirl route</h2>
-<h3>https://ai.helium.ws/pup/[prompt]</h3>
+	const handlecg = () => {
+		loading = true;
+		goto(`/cat/${cgInput}`);
+	};
+
+	const handlepg = () => {
+		loading = true;
+		goto(`/pup/${pgInput}`);
+	};
+</script>
+
+<div class={loading ? 'fullscreen' : 'hidden'}>
+	<p>loading :3</p>
+</div>
+
+<h1>the worst ai ever done</h1>
+
+<input type="text" bind:value={cgInput} />
+<button on:click|preventDefault={handlecg}>prompt catgirl ai</button>
+<br />
+<input type="text" bind:value={pgInput} />
+<button on:click|preventDefault={handlepg}>prompt puppygirl ai</button>
+
+<style>
+	.hidden {
+		display: none;
+	}
+
+	.fullscreen {
+		position: absolute;
+		top: 0;
+		left: 0;
+		width: 100vw;
+		height: 100vh;
+		display: flex;
+		justify-content: center;
+		align-items: center;
+		background: rgba(0, 0, 0, 0.75);
+		color: white;
+	}
+</style>
