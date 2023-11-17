d6ebea8ef84092652f2da925cdff1b21d3fd8c6e
embed <brydon@lecla.ir>
2023-11-17 11:12:47 -0500

    [~] new landing page

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,1 +1,7 @@
-<h1>api: https://ai.helium.ws/(prompt)</h1>
+<h1>helium ai</h1>
+
+<h2>catgirl route</h2>
+<h3>https://ai.helium.ws/cat/[prompt]</h3>
+
+<h2>puppygirl route</h2>
+<h3>https://ai.helium.ws/pup/[prompt]</h3>
